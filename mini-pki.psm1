df6e7b54ffe0fb920b36eb80d2f5768c36e91721
Administrateur@EPITAF.LOCAL <Administrateur@EPITAF.LOCAL>
2020-03-31 05:29:31 +0200

    fix script hanging

--- mini-pki.psm1
+++ mini-pki.psm1
@@ -26,10 +26,13 @@ $directories = @($ca_root_dir, $ca_private_dir, $ca_public_dir, $ca_cert_dir, $c
 
 function create-ca {
   New-Item -ItemType "directory" -Path $directories -Force
+  
+  $subject = @("C=FR", "O=EPITA", "OU=SRS", "E=srs@epita.com", "CN=root")
+  $subject_string = "/" + ($subject -join "/")
 
-  openssl.exe genrsa -des3 -out "$ca_private_dir/caprivatekey.pem" 2048
-  openssl.exe req -x509 -new -nodes -key "$ca_private_dir/caprivatekey.pem" -sha256 -days 3000 -out "$ca_cert_dir/cacert.pem"
- 
+  openssl req -new -outform "PEM" -sha256 -newkey rsa:2048 -subj $subject_string -keyout "$ca_private_dir/caprivatekey.pem" -out "$ca_cert_dir/cacert.pem" -extensions "v3_ca" -days 3000 -x509  
+
+  openssl rsa -in "$ca_private_dir/caprivatekey.pem" -pubout -out "$ca_public_dir/capublickey.pem"
   icacls ($current_drive_letter + ":/$ca_private_dir") /grant:r Administrateur:F /T
 }
 
